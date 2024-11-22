88a8119b1ae2376a5c47d7fff71dc3f65dfb6d1b
Chaimae-ch1 <chaaibi.chaimae1@gmail.com>
2024-11-22 12:16:37 +0100

    UserDetails.vue

--- frontend/src/router/index.js
+++ frontend/src/router/index.js
@@ -1,13 +1,11 @@
+import AdminDashboard from '@/views/AdminDashboard.vue';
+import MaterialDetail from '@/views/MaterialDetail.vue';
+import MaterialPage from '@/views/MaterialPage.vue';
+import UserDetails from '@/views/UserDetails.vue';
+import UserLogin from '@/views/UserLogin.vue';
+import UserManagement from '@/views/UserManagement.vue';
 
 import { createRouter, createWebHistory } from 'vue-router';
-import UserLogin from '@/views/UserLogin.vue';
-import AdminDashboard from '@/views/AdminDashboard.vue';
-import UserManagement from '@/views/UserManagement.vue';
-import MaterialPage from '@/views/MaterialPage.vue';
-import MaterialDetail from '@/views/MaterialDetail.vue';
-
-
-
 const routes = [
   {
     path: '/',
@@ -29,16 +27,24 @@ const routes = [
     name: 'MaterialPage',
     component: MaterialPage
   },
-  { path: '/material/:id',
+  {
+    path: '/material/:id',
     name: 'MaterialDetail',
     component: MaterialDetail,
-    props: true },
-
+    props: true
+  },
+  {
+    path: '/user/:id',
+    name: 'UserDetails',
+    component: UserDetails,
+    props: true
+  },
 ];
 
 const router = createRouter({
   history: createWebHistory(),
   routes
 });
+
 
 export default router;
