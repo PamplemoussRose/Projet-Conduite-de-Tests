5e176fb79315ee86837be31a648f553806a1dfd7
Jaafar Ghiffi <122598525+JaafarG@users.noreply.github.com>
2024-11-22 14:33:54 +0100

    Update index.js

--- frontend/src/router/index.js
+++ frontend/src/router/index.js
@@ -6,17 +6,6 @@ import UserLogin from '@/views/UserLogin.vue';
 import UserManagement from '@/views/UserManagement.vue';
 
 import { createRouter, createWebHistory } from 'vue-router';
-<<<<<<< HEAD
-=======
-import UserLogin from '@/views/UserLogin.vue';
-import AdminDashboard from '@/views/AdminDashboard.vue';
-import UserManagement from '@/views/UserManagement.vue';
-import EquipmentDetail from "@/views/EquipmentDetail.vue";
-import EquipmentPage from "@/views/EquipmentPage.vue";
-
-
-
->>>>>>> f822a57812af95c45cbaaeb5ffde68f06c06f864
 const routes = [
   {
     path: '/',
